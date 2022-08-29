f2aa688b27b6da69c110399c46ce099cec848d7a
Maxim Petrov <max.zh.petrov@gmail.com>
2022-08-29 16:34:58 +0300

    ref btn

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import PriceFilter from '../price-filter';
 import FilterTransfers from '../filter-transfers';
 import TicketsList from '../tickets-list';
-import * as action from '../../redux/show-more/show-more-actions';
 
 import './app.scss';
 import logo from './Logo.svg';
 
-const App = ({ addFive }) => {
+const App = () => {
   return (
     <main className="content">
       <img className="aviasales-logo" src={logo} alt="logo" />
       <PriceFilter />
       <FilterTransfers />
       <TicketsList />
-      <input type="button" className="btn-more" defaultValue="Показать еще 5 билетов!" onClick={addFive} />
     </main>
   );
 };
 
-export default connect(null, action)(App);
+export default App;
--- src/components/tickets-list/tickets-list.js
+++ src/components/tickets-list/tickets-list.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import * as action from '../../redux/tickets/tickets-action';
+import { addFive } from '../../redux/show-more/show-more-actions';
 import Ticket from '../ticket';
 import Loader from '../loader/loader';
 
@@ -9,27 +10,39 @@ import { PriceSorter, TransferSorter } from './tickets-sorter';
 
 import './tickets-list.scss';
 
-const TicketsList = ({ tickets, start, loader }) => {
+const TicketsList = ({ tickets, start, loader, addFive }) => {
   useEffect(() => {
     start();
   }, []);
   const list = tickets.map(({ ...props }, index) => {
     return <Ticket key={index} {...props} />;
   });
+  const empty = list.length === 0;
   return (
-    <div className="tickets-list">
-      {loader && <Loader />}
-      {list}
-      {!loader && !list.length && 'Рейсов, подходящих под заданные фильтры, не найдено'}
-    </div>
+    <>
+      <div className="tickets-list">
+        {loader && <Loader />}
+        {list}
+        {!loader && empty && 'Рейсов, подходящих под заданные фильтры, не найдено'}
+      </div>
+      {!empty && <input type="button" className="btn-more" defaultValue="Показать еще 5 билетов!" onClick={addFive} />}
+    </>
   );
 };
 
-const mapDispatchToProps = ({ transfersReducer, pricerReducer, ticketReducer, showMoreReducer }) => {
+const mapStateToProps = ({ transfersReducer, pricerReducer, ticketReducer, showMoreReducer }) => {
   const filtredArr = PriceSorter(pricerReducer, TransferSorter(transfersReducer, ticketReducer.tickets));
   return {
     loader: ticketReducer.loader,
     tickets: filtredArr.slice(0, showMoreReducer),
   };
 };
-export default connect(mapDispatchToProps, action)(TicketsList);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addFive: () => dispatch(addFive()),
+    start: () => dispatch(action.start()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TicketsList);
